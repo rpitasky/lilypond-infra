fdd9498d8bc0e423539c00d8fba115d1fd862459
Ryan Pitasky <ryanpitasky+github@gmail.com>
2026-08-20 20:49:12 -0400

    Build attempt 7

--- lilypond_infra/accounts/migrations/0001_create_admin_group.py
+++ lilypond_infra/accounts/migrations/0001_create_admin_group.py
@@ -6,11 +6,11 @@ from django.contrib.contenttypes.models import ContentType
 def create_staff_group(apps, schema_editor):
     group, created = Group.objects.get_or_create(name="Admin")
 
-    book_ct = ContentType.objects.get(app_label="file_upload", model="book")
-    transcription_ct = ContentType.objects.get(
+    book_ct, _ = ContentType.objects.get_or_create(app_label="file_upload", model="book")
+    transcription_ct, _ = ContentType.objects.get_or_create(
         app_label="file_upload", model="transcription"
     )
-    revision_ct = ContentType.objects.get(app_label="file_upload", model="revision")
+    revision_ct, _ = ContentType.objects.get_or_create(app_label="file_upload", model="revision")
 
     permissions = Permission.objects.filter(
         content_type__in=[book_ct, transcription_ct, revision_ct]
@@ -27,6 +27,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("file_upload", "0002_alter_revision_index"),
         ("auth", "0012_alter_user_first_name_max_length"),
+        ("contenttypes", "0002_remove_content_type_name"),
     ]
 
     operations = [
