08972ca72991acd7a873f550435f9701c0dcf692
Ryan Pitasky <ryanpitasky+github@gmail.com>
2026-08-21 00:08:42 -0400

    Build attempt

--- lilypond_infra/lilypond_infra/settings.py
+++ lilypond_infra/lilypond_infra/settings.py
@@ -155,6 +155,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
